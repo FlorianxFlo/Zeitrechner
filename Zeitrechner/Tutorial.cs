3408373a725a6461f0ef122b6616785ee9432208
Florian Bauer <Florian.Tobias.Bauer@adac.de>
2026-06-19 11:34:27 +0200

    Added Tutorial Class

---
+++ Zeitrechner/Tutorial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zeitrechner
+{
+    public class Tutorial
+    {
+        public Tutorial()
+        {
+            Console.WriteLine("Tutorial erfolgreich gestartet!");
+
+        }
+    }
+}
